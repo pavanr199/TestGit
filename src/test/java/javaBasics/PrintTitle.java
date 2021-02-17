ef4a7f8877c2888dea285513f9d919883eb1c8da
HP <HP@DESKTOP-S4I1B9H>
2021-02-17 10:24:50 +0530

    first writencode

--- src/test/java/javaBasics/PrintTitle.java
+++ src/test/java/javaBasics/PrintTitle.java
@@ -5,6 +5,8 @@ public class PrintTitle {
 	public static void main(String[] args) {
 		
  System.out.println("first time get project from git");
+ 
+ System.out.println("get project from git is======== successfull");
 	}
 
 }
